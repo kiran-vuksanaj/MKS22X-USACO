d678f0d66f45c2c7f62dab61f253ea88b6d0110b
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-05 10:10:48 -0500

    data access from scanner and headers for getMap and stomp methods

--- USACO.java
+++ USACO.java
@@ -4,10 +4,23 @@ public class USACO{
   public static int bronze(String filename){
     try{
       Scanner sca = new Scanner( new File(filename) );
-      return -1;
+      int r = sca.nextInt();
+      int c = sca.nextInt();
+      int e = sca.nextInt();
+      int n = sca.nextInt();
+      int[][] map = getMap(sca,r,c);
+      while(sca.hasNextInt()){
+        stomp(sca.nextInt(),sca.nextInt(),sca.nextInt(),map);
+      }
     }catch(FileNotFoundException e){
 
     }
     return -1;
+  }
+  public static int[][] getMap(Scanner sca,int r,int c){
+    return null;
+  }
+  public static void stomp(int r,int c,int depth,int[][] map){
+    
   }
 }
