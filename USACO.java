86878219b36f3ed4283a76b8d9638e4816f03f2b
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-05 10:05:58 -0500

    file access

---
+++ USACO.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class USACO{
+  public static int bronze(String filename){
+    try{
+      Scanner sca = new Scanner( new File(filename) );
+      return -1;
+    }catch(FileNotFoundException e){
+
+    }
+    return -1;
+  }
+}
